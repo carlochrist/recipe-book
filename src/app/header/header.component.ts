2160afad6f60d4f1604d72752335d4b2eb4ffb64
Carlo Christ <carlo.christ@com-software.de>
2022-01-23 13:01:27 +0100

    added auth | general improvements

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,16 +1,40 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { AuthService } from '../auth/auth.service';
+import { DataStorageService } from '../shared/data-storage.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
   collapsed = true;
+  private userSub: Subscription;
+  isAuthenticated = false;
 
-  constructor() { }
+  constructor(
+    private dataStorageService: DataStorageService,
+    private authService: AuthService
+  ) {}
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.userSub = this.authService.user.subscribe((user) => {
+      this.isAuthenticated = !!user;
+      console.log(!user);
+      console.log(!!user);
+    });
   }
 
+  onSaveData() {
+    this.dataStorageService.storeRecipes();
+  }
+
+  onFetchData() {
+    this.dataStorageService.fetchRecipes().subscribe();
+  }
+
+  onDestroy() {
+    this.userSub.unsubscribe();
+  }
 }
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -8,27 +8,28 @@ import { Recipe } from './recipe.model';
 export class RecipeService {
   recipesChanged = new Subject<Recipe[]>();
 
-  private recipes: Recipe[] = [
-    new Recipe(
-      // 1,
-      'A tasty Schnitzel',
-      'Just awesome!',
-      'https://upload.wikimedia.org/wikipedia/commons/3/37/Schnitzel-Mit-Pommes.jpg',
-      [new Ingredient('Meat', 1), new Ingredient('French fries', 20)]
-    ),
-    new Recipe(
-      // 2,
-      'Big fat burger',
-      "You won't believe how tasty it is!",
-      'https://img.chefkoch-cdn.de/rezepte/1287261234164379/bilder/917534/crop-960x640/wuerzige-hamburger.jpg',
-      [
-        new Ingredient('Buns', 1),
-        new Ingredient('Meat', 1),
-        new Ingredient('Tomato', 2),
-        new Ingredient('Salad', 1),
-      ]
-    ),
-  ];
+  // private recipes: Recipe[] = [
+  //   new Recipe(
+  //     // 1,
+  //     'A tasty Schnitzel',
+  //     'Just awesome!',
+  //     'https://upload.wikimedia.org/wikipedia/commons/3/37/Schnitzel-Mit-Pommes.jpg',
+  //     [new Ingredient('Meat', 1), new Ingredient('French fries', 20)]
+  //   ),
+  //   new Recipe(
+  //     // 2,
+  //     'Big fat burger',
+  //     "You won't believe how tasty it is!",
+  //     'https://img.chefkoch-cdn.de/rezepte/1287261234164379/bilder/917534/crop-960x640/wuerzige-hamburger.jpg',
+  //     [
+  //       new Ingredient('Buns', 1),
+  //       new Ingredient('Meat', 1),
+  //       new Ingredient('Tomato', 2),
+  //       new Ingredient('Salad', 1),
+  //     ]
+  //   ),
+  // ];
+  private recipes: Recipe[] = [];
 
   constructor(private shoppingListService: ShoppingListService) {}
 
@@ -51,6 +52,11 @@ export class RecipeService {
 
   addRecipe(recipe: Recipe) {
     this.recipes.push(recipe);
+    this.recipesChanged.next(this.recipes.slice());
+  }
+
+  setRecipes(newRecipes: Recipe[]) {
+    this.recipes = newRecipes;
     this.recipesChanged.next(this.recipes.slice());
   }
 
